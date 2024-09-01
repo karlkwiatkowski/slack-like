19248cca6ec5790bb55bfc265bc91bb8dfcb18dc
Karl Kwiatkowski <karl@Dobbys-MacBook.local>
2024-08-31 22:18:26 -0400

    Initial commit

---
+++ src/main/java/com/mambo/slacklike/SlacklikeApplication.java
@@ -0,0 +1,13 @@
+package com.mambo.slacklike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlacklikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlacklikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mambo/slacklike/SlacklikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mambo.slacklike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlacklikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
